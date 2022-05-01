caafe7f9a3f873bb8c72c37c78d3a2c4d58500ac
ismailcakici <ismailcakici30@gmail.com>
2022-05-01 23:16:37 +0300

    ilk commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       title: 'Flutter sena',
-
-
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
